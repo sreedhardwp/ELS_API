754d12c80c351711dac8c2f534d0592f10cc10e4
nivaskumar3010@outlook.com <nivaskumar3010@outlook.com>
2026-05-14 11:02:01 +0530

    allowcreds

--- EmployeeLeaveSystem_BackEnd/Program.cs
+++ EmployeeLeaveSystem_BackEnd/Program.cs
@@ -51,7 +51,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular", policy =>
         policy.WithOrigins("http://localhost:4200")
               .AllowAnyHeader()
-              .AllowAnyMethod());
+              .AllowAnyMethod()
+              .AllowCredentials());
 });
 
 builder.Services.AddControllers();
